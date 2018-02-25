6af30d0963224533aa9db50d1c260a0f71253e59
Jonathan Oaks <joaks001@ucr.edu>
2018-02-25 12:55:16 -0800

    Updated test programs

---
+++ tests/precedence_test.sh
@@ -0,0 +1,11 @@
+#Tests the precedence commands
+
+echo
+echo "Testing precedence commands"
+
+echo
+../bin/rshell.out < "precedence_test.txt"
+echo
+echo "Test Complete"
+echo
+
---
+++ tests/test_test.sh
@@ -0,0 +1,11 @@
+#Tests the "test" command
+
+echo
+echo "Testing \"test\" commands"
+
+echo
+../bin/rshell.out < "test_test.txt"
+echo
+echo "Test Complete"
+echo
+
